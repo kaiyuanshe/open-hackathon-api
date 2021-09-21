b016acfd58adb7a540e2f69252ff3bb970d4c8ff
Junbo Wang <juniwang@microsoft.com>
2021-09-21 20:18:54 +0800

    Merge pull request #4 from juniwang/juniwang/dev
    update comments

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ExperimentManagement.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ExperimentManagement.cs
@@ -86,7 +86,7 @@ namespace Kaiyuanshe.OpenHackathon.Server.Biz
             };
             await StorageContext.ExperimentTable.InsertOrReplaceAsync(entity, cancellationToken);
 
-            // TODO call k8s api
+            // TODO call k8s apis
             entity = await StorageContext.ExperimentTable.RetrieveAsync(entity.PartitionKey, entity.RowKey, cancellationToken);
             var context = new ExperimentContext
             {
