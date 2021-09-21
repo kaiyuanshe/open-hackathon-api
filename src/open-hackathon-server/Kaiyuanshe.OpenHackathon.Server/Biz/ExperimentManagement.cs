c4aca217a1e5ace41ccbc6c142b2bfacac3ef81c
Junbo Wang <wangjunbo924@hotmail.com>
2021-09-21 20:36:13 +0800

    update comments 2

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ExperimentManagement.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ExperimentManagement.cs
@@ -86,7 +86,7 @@ namespace Kaiyuanshe.OpenHackathon.Server.Biz
             };
             await StorageContext.ExperimentTable.InsertOrReplaceAsync(entity, cancellationToken);
 
-            // TODO call k8s apis
+            // TODO call k8s api
             entity = await StorageContext.ExperimentTable.RetrieveAsync(entity.PartitionKey, entity.RowKey, cancellationToken);
             var context = new ExperimentContext
             {
