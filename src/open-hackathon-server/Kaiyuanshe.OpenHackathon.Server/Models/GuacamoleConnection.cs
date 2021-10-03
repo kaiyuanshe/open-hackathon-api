57d6d26b602eb30fd3facc922643bdffe5c4bf5f
Junbo Wang <juniwang@microsoft.com>
2021-10-03 22:07:42 +0800

    vnc model and method to query k8s exp status (#11)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Models/GuacamoleConnection.cs
@@ -0,0 +1,51 @@
+﻿namespace Kaiyuanshe.OpenHackathon.Server.Models
+{
+    /// <summary>
+    /// info for remote connection like vnc, ssh and rdp.
+    /// </summary>
+    public abstract class GuacamoleConnection
+    {
+        public string name { get; set; }
+
+        /// <summary>
+        /// protocol for remote connection
+        /// </summary>
+        /// <example>vnc</example>
+        public IngressProtocol protocol { get; internal set; }
+    }
+
+    public class GuacamoleConnectionList : ResourceList<GuacamoleConnection>
+    {
+        /// <summary>
+        /// a list of connection info.
+        /// </summary>
+        public override GuacamoleConnection[] value { get; set; }
+    }
+
+    /// <summary>
+    /// Vnc Connection settings for Guacamole.
+    /// See also: https://guacamole.apache.org/doc/gug/configuring-guacamole.html#vnc
+    /// </summary>
+    public class VncConnection : GuacamoleConnection
+    {
+        /// <summary>
+        /// The hostname or IP address of the VNC server Guacamole should connect to.
+        /// </summary>
+        public string hostname { get; internal set; }
+
+        /// <summary>
+        /// The port the VNC server is listening on, usually 5900 or 5900 + display number. For example, if your VNC server is serving display number 1 (sometimes written as :1), your port number here would be 5901.
+        /// </summary>
+        public string port { get; internal set; }
+
+        /// <summary>
+        /// The username to use when attempting authentication, if any. This parameter is optional.
+        /// </summary>
+        public string username { get; internal set; }
+
+        /// <summary>
+        /// The password to use when attempting authentication, if any. This parameter is optional.
+        /// </summary>
+        public string password { get; internal set; }
+    }
+}
