91ef0bf22f672d5d63e8b84139786e89c8c4359a
Junbo Wang <juniwang@microsoft.com>
2021-12-11 21:37:54 +0800

    autofac extensions for tables v2 (#22)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/DependencyInjection/ContainerBuilderExtension.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Kaiyuanshe.OpenHackathon.Server.DependencyInjection
+{
+    public static class ContainerBuilderExtension
+    {
+        /// <summary>
+        /// Register all un-abstract sub types as self without interface support.
+        /// Generic not supported. 
+        /// </summary>
+        public static void RegisterSubTypes(this ContainerBuilder builder, Type abstractType, params Assembly[] assemblies)
+        {
+            if (abstractType.IsGenericType)
+                return;
+
+            builder.RegisterTypes(abstractType.SubTypes(assemblies)).PropertiesAutowired().SingleInstance();
+        }
+
+        /// <summary>
+        /// Register all un-abstract sub types as direct interface. 
+        /// Not registered if no direct interface.
+        /// DONT use this method if any of its direct interfaces has multiple implementations.
+        /// </summary>
+        public static void RegisterSubTypesAsDirectInterfaces(this ContainerBuilder builder, Type abstractType, params Assembly[] assemblies)
+        {
+            var subTypes = abstractType.IsGenericType ? abstractType.GenericSubTypes(assemblies) : abstractType.SubTypes(assemblies);
+            foreach (var subType in subTypes)
+            {
+                var directInterfaces = subType.GetInterfaces(false);
+                if (directInterfaces.Count() > 0)
+                {
+                    builder.RegisterType(subType).AsSelf().SingleInstance().PropertiesAutowired().As(directInterfaces.ToArray());
+                }
+            }
+        }
+    }
+}
---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/DependencyInjection/ContainerBuilderExtensionTests.cs
@@ -0,0 +1,80 @@
+﻿using Autofac;
+using Kaiyuanshe.OpenHackathon.Server.DependencyInjection;
+using NUnit.Framework;
+using System;
+
+namespace Kaiyuanshe.OpenHackathon.ServerTests.DependencyInjection
+{
+    class ContainerBuilderExtensionTests
+    {
+        [Test]
+        public void RegisterSubTypes()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterSubTypes(typeof(ITestA));
+
+            var container = builder.Build();
+            Assert.IsFalse(container.IsRegistered(typeof(ITestA)));
+            Assert.IsFalse(container.IsRegistered(typeof(TestABase)));
+            Assert.IsTrue(container.IsRegistered(typeof(TestASubA)));
+            Assert.IsTrue(container.IsRegistered(typeof(TestASubB)));
+        }
+
+        [Test]
+        public void RegisterSubTypesAsDirectInterfaces()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterSubTypesAsDirectInterfaces(typeof(ITestB));
+
+            var container = builder.Build();
+            Assert.IsFalse(container.IsRegistered(typeof(ITestB)));
+            Assert.IsFalse(container.IsRegistered(typeof(TestBBase)));
+
+            // no interface
+            Assert.IsFalse(container.IsRegistered(typeof(TestBSubC)));
+
+            // single
+            AssertResolvable(container, typeof(ITestBSubB), typeof(TestBSubB));
+
+            // multiple
+            AssertResolvable(container, typeof(ITestBSubA1), typeof(TestBSubA));
+            AssertResolvable(container, typeof(ITestBSubA2), typeof(TestBSubA));
+        }
+
+        [Test]
+        public void RegisterGenericSubTypesAsDirectInterfaces()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterSubTypesAsDirectInterfaces(typeof(ITestC<>));
+
+            var container = builder.Build();
+            Assert.IsFalse(container.IsRegistered(typeof(ITestC<>)));
+            Assert.IsFalse(container.IsRegistered(typeof(TestCBase<>)));
+
+            // no interface
+            Assert.IsFalse(container.IsRegistered(typeof(TestCSubA)));
+
+            // single interface
+            AssertResolvable(container, typeof(ITestCSubB), typeof(TestCSubB));
+
+            // multiple interfaces
+            AssertResolvable(container, typeof(ITestCSubC1), typeof(TestCSubC));
+            AssertResolvable(container, typeof(ITestCSubC2), typeof(TestCSubC));
+
+            // sub-sub
+            AssertResolvable(container, typeof(ITestCSubD), typeof(TestCSubDSub));
+
+            // generic sub
+            AssertResolvable(container, typeof(IWLogSubTable), typeof(WlogSubTable));
+        }
+
+        private void AssertResolvable(IContainer container, Type resolveType, Type expectedInstanceType)
+        {
+            Assert.IsTrue(container.IsRegistered(resolveType));
+            Assert.IsTrue(container.IsRegistered(expectedInstanceType));
+            var resolved = container.Resolve(resolveType);
+            Assert.IsNotNull(resolved);
+            Assert.AreEqual(expectedInstanceType, resolved.GetType());
+        }
+    }
+}
