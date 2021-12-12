c0b5316de690b543659a71184788291d5557e8a8
Junbo Wang <juniwang@microsoft.com>
2021-12-12 22:26:23 +0800

    add StorageClientBase

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/StorageClientBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Storage
+{
+    public abstract class StorageClientBase
+    {
+        public IStorageCredentialProvider StorageCredentialProvider { get; set; }
+
+        public abstract string StorageName { get; }
+
+        protected IDictionary<string, object> GetMessageProperties()
+        {
+            Dictionary<string, object> properties = new Dictionary<string, object>();
+            string traceId = Activity.Current?.Id ?? string.Empty;
+            properties.Add(HttpHeaderNames.TraceId, traceId);
+            return properties;
+        }
+    }
+}
