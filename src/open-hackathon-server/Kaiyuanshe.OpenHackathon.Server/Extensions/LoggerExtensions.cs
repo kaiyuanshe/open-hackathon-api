64dc9a22d99a01a18cf31332e664875eb785630b
Junbo Wang <juniwang@microsoft.com>
2021-12-15 09:14:33 +0800

    adding more table APIs in v2 client

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Extensions/LoggerExtensions.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Extensions/LoggerExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Kaiyuanshe.OpenHackathon.Server
 {
--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Tables/AzureTableV2.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Tables/AzureTableV2.cs
@@ -5,6 +5,7 @@ using Azure.Data.Tables;
 using Kaiyuanshe.OpenHackathon.Server.Extensions;
 using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +14,18 @@ namespace Kaiyuanshe.OpenHackathon.Server.Storage.Tables
 {
     public interface IAzureTableV2<TEntity> where TEntity : BaseTableEntity, new()
     {
+        Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task MergeAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task InsertOrMergeAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task RetrieveAndMergeAsync(string partitionKey, string rowKey, Action<TEntity> func, CancellationToken cancellationToken = default);
+        Task InsertOrReplaceAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task ReplaceAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(string partitionKey, string rowKey, CancellationToken cancellationToken = default);
+        Task<TEntity> RetrieveAsync(string partitionKey, string rowKey, CancellationToken cancellationToken = default);
         Task<IEnumerable<TEntity>> QueryEntitiesAsync(string filter, IEnumerable<string> select = null, CancellationToken cancellationToken = default);
+        Task ExecuteQueryAsync(string filter, Action<TEntity> action, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default);
+        Task ExecuteQueryAsync(string filter, Func<TEntity, Task> asyncAction, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default);
+        Task ExecuteQueryInParallelAsync(string filter, Func<TEntity, Task> asyncAction, int maxParallelism = 5, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default);
     }
 
     public abstract class AzureTableV2<TEntity> : StorageClientBase, IAzureTableV2<TEntity> where TEntity : BaseTableEntity, new()
@@ -25,6 +37,136 @@ namespace Kaiyuanshe.OpenHackathon.Server.Storage.Tables
         protected AzureTableV2(ILogger logger)
         {
             this.logger = logger;
+        }
+
+        public virtual async Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.AddEntityAsync(entity.ToTableEntity(), cancellationToken);
+                    entity.ETag = resp.Headers.ETag.Value.ToString();
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task MergeAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.UpdateEntityAsync(entity.ToTableEntity(), new ETag(entity.ETag), TableUpdateMode.Merge, cancellationToken);
+                    entity.ETag = resp.Headers.ETag.Value.ToString();
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task InsertOrMergeAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.UpsertEntityAsync(entity.ToTableEntity(), TableUpdateMode.Merge, cancellationToken);
+                    entity.ETag = resp.Headers.ETag.Value.ToString();
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task RetrieveAndMergeAsync(string partitionKey, string rowKey, Action<TEntity> func, CancellationToken cancellationToken = default)
+        {
+            var entity = await RetrieveAsync(partitionKey, rowKey, cancellationToken);
+            func(entity);
+            await MergeAsync(entity, cancellationToken);
+        }
+
+        public virtual async Task InsertOrReplaceAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.UpsertEntityAsync(entity.ToTableEntity(), TableUpdateMode.Replace, cancellationToken);
+                    entity.ETag = resp.Headers.ETag.Value.ToString();
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task ReplaceAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.UpdateEntityAsync(entity.ToTableEntity(), new ETag(entity.ETag), TableUpdateMode.Replace, cancellationToken);
+                    entity.ETag = resp.Headers.ETag.Value.ToString();
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task DeleteAsync(string partitionKey, string rowKey, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    await client.DeleteEntityAsync(partitionKey, rowKey, default, cancellationToken);
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task<TEntity> RetrieveAsync(string partitionKey, string rowKey, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var resp = await client.GetEntityAsync<TableEntity>(partitionKey, rowKey, null, cancellationToken);
+                    return resp.Value.ToBaseTableEntity<TEntity>();
+                }
+                catch (RequestFailedException ex)
+                {
+                    if (ex.Status == 404)
+                    {
+                        return default(TEntity);
+                    }
+
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
         }
 
         public async Task<IEnumerable<TEntity>> QueryEntitiesAsync(string filter, IEnumerable<string> select = null, CancellationToken cancellationToken = default)
@@ -44,6 +186,57 @@ namespace Kaiyuanshe.OpenHackathon.Server.Storage.Tables
                 }
             }
             return entities;
+        }
+
+        public virtual async Task ExecuteQueryAsync(string filter, Action<TEntity> action, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var query = client.QueryAsync<TableEntity>(filter, limit, select, cancellationToken);
+                    await query.ForEach(entity => action(entity.ToBaseTableEntity<TEntity>()), limit, cancellationToken);
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task ExecuteQueryAsync(string filter, Func<TEntity, Task> asyncAction, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var query = client.QueryAsync<TableEntity>(filter, limit, select, cancellationToken);
+                    await query.ForEach(entity => asyncAction(entity.ToBaseTableEntity<TEntity>()), limit, cancellationToken);
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
+        }
+
+        public virtual async Task ExecuteQueryInParallelAsync(string filter, Func<TEntity, Task> asyncAction, int maxParallelism = 5, int? limit = null, IEnumerable<string> select = null, CancellationToken cancellationToken = default)
+        {
+            var client = await GetTableClientAsync(cancellationToken);
+            using (HttpPipeline.CreateHttpMessagePropertiesScope(GetMessageProperties()))
+            {
+                try
+                {
+                    var query = client.QueryAsync<TableEntity>(filter, limit, select, cancellationToken);
+                    await query.ParallelForEachAsync(entity => asyncAction(entity.ToBaseTableEntity<TEntity>()), maxParallelism, limit, cancellationToken);
+                }
+                catch (RequestFailedException ex)
+                {
+                    throw new AzureStorageException(ex.Status, ex.Message, ex.ErrorCode, ex);
+                }
+            }
         }
 
         private async Task<TableClient> GetTableClientAsync(CancellationToken cancellationToken)
