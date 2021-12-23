408498a20e4d677b457007a77cf032c97e8f92a5
Junbo Wang <juniwang@microsoft.com>
2021-12-23 09:38:17 +0800

    cleanup MockHelper (#47)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/MockHelper.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/MockHelper.cs
@@ -22,12 +22,6 @@ namespace Kaiyuanshe.OpenHackathon.ServerTests
             return querySegment;
         }
 
-        public static Page<T> CreatePage<T>(List<T> result, (string NextPartitionKey, string NextRowKey) continuationToken)
-        {
-            var ct = TableQueryHelper.ToContinuationToken(continuationToken);
-            return CreatePage<T>(result, ct);
-        }
-
         public static Page<T> CreatePage<T>(List<T> result, string continuationToken)
         {
             return Page<T>.FromValues(result, continuationToken, null);
