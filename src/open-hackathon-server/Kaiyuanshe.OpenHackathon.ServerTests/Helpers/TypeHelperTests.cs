9f7dcd419d70997fce78fe721455e68a240afa81
Junbo Wang <juniwang@microsoft.com>
2021-12-27 19:54:39 +0800

    cleanup TypeHelper

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Helpers/TypeHelperTests.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Helpers/TypeHelperTests.cs
@@ -1,12 +1,9 @@
 ﻿using Authing.ApiClient.Types;
 using Kaiyuanshe.OpenHackathon.Server;
 using Kaiyuanshe.OpenHackathon.Server.Models;
-using Microsoft.WindowsAzure.Storage.Table;
-using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Kaiyuanshe.OpenHackathon.ServerTests.Helpers
 {
@@ -55,64 +52,6 @@ namespace Kaiyuanshe.OpenHackathon.ServerTests.Helpers
             Assert.AreEqual(date.ToString("o"), user2.TokenExpiredAt);
             Assert.AreEqual(identities, user2.Identities);
             //Assert.AreEqual(roles, user2.Roles);
-        }
-
-        [Test]
-        public void ToTableEntityTest()
-        {
-            var identities = new List<Identity> { new Identity { Openid = "openid", Provider = "provider" } };
-            UserInfo user = new UserInfo
-            {
-                Name = "name",
-                Blocked = true,
-                LoginsCount = 32,
-                Identities = identities,
-                createdAt = DateTime.UtcNow,
-            };
-
-            var tableEntity = user.ToTableEntity("pk", "rk", (t) =>
-            {
-                t.Properties["foo"] = new EntityProperty("bar");
-            });
-
-            Assert.AreEqual("name", tableEntity.Properties["Name"].StringValue);
-            Assert.AreEqual(true, tableEntity.Properties["Blocked"].BooleanValue);
-            Assert.AreEqual(32, tableEntity.Properties["LoginsCount"].Int32Value);
-            Assert.AreEqual(JsonConvert.SerializeObject(identities), tableEntity.Properties["Identities"].StringValue);
-            Assert.AreEqual("bar", tableEntity.Properties["foo"].StringValue);
-            Assert.IsTrue(tableEntity.Properties.ContainsKey("createdAt"));
-            Assert.IsFalse(tableEntity.Properties.ContainsKey("updatedAt"));
-        }
-
-        [Test]
-        public void ToModelTest()
-        {
-            var identities = new List<Identity> { new Identity { Openid = "openid", Provider = "provider" } };
-            var str = JsonConvert.SerializeObject(identities);
-            DynamicTableEntity tableEntity = new DynamicTableEntity("pk", "rk")
-            {
-                Properties = new Dictionary<string, EntityProperty>
-                {
-                    { "LoginsCount", new EntityProperty(10) },
-                    { "Name", new EntityProperty("name") },
-                    { "Blocked", new EntityProperty(true) },
-                    { "Identities", new EntityProperty(str) },
-                }
-            };
-
-            var user = new UserInfo();
-            user = tableEntity.ToModel(user, (u) =>
-            {
-                u.Birthdate = "birthdate";
-            });
-
-            Assert.AreEqual("birthdate", user.Birthdate);
-            Assert.AreEqual(10, user.LoginsCount);
-            Assert.AreEqual("name", user.Name);
-            Assert.AreEqual(true, user.Blocked);
-            Assert.AreEqual(1, user.Identities.Count());
-            Assert.AreEqual("openid", user.Identities.First().Openid);
-            Assert.IsNull(user.City);
         }
 
         public interface IInheritTest { }
