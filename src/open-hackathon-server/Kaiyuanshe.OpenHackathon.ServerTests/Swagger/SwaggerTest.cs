744e760e44c328a8ff29176f6e58d0ac2efd4e0f
Junbo Wang <juniwang@microsoft.com>
2021-12-28 12:36:50 +0800

    implement azure blob container v2 (#58)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Swagger/SwaggerTest.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.ServerTests/Swagger/SwaggerTest.cs
@@ -3,7 +3,6 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Text.Json;
 
 namespace Kaiyuanshe.OpenHackathon.ServerTests.Swagger
