ef9f890479ae6ea0570adbce71575f03a39d04d7
Junbo Wang <juniwang@microsoft.com>
2022-03-13 11:04:39 +0800

    return endpoint for guacamole (#133)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Constants.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Constants.cs
@@ -8,4 +8,9 @@
     {
         public const string TraceId = "x-oph-trace-id";
     }
+
+    public static class Guacamole
+    {
+        public const string UriFormat = "https://guacamole.kaiyuanshe.cn/guacamole/#/?hackathon={0}&experiment={1}&token=";
+    }
 }
