0ab8ec726e235e140e3aacaaab241a619ee078fd
Junbo Wang <juniwang@microsoft.com>
2022-06-22 18:35:25 +0800

    add model/entity for organizer (#246)

---
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Storage/Entities/OrganizerEntity.cs
@@ -0,0 +1,39 @@
+﻿using Kaiyuanshe.OpenHackathon.Server.Models;
+
+namespace Kaiyuanshe.OpenHackathon.Server.Storage.Entities
+{
+    /// <summary>
+    /// Entity for hackathon organizers.
+    /// 
+    /// PK: hackathonName; 
+    /// RK: Guid
+    /// </summary>
+    public class OrganizerEntity : BaseTableEntity
+    {
+        /// <summary>
+        /// name of Hackathon. PartitionKey
+        /// </summary>
+        [IgnoreEntityProperty]
+        public string HackathonName
+        {
+            get { return PartitionKey; }
+        }
+
+        /// <summary>
+        /// id of organizer. RowKey
+        /// </summary>
+        [IgnoreEntityProperty]
+        public string Id
+        {
+            get
+            {
+                return RowKey;
+            }
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public OrganizerType target { get; set; }
+        public PictureInfo logo { get; set; }
+    }
+}
