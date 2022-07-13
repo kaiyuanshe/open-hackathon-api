0bf702256ec8e7656b238ed6e79b4a478d2f0841
Junbo Wang <juniwang@microsoft.com>
2022-07-13 22:11:59 +0800

    add mgmt client v2

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ManagementClientBase.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/Biz/ManagementClientBase.cs
@@ -2,6 +2,7 @@
 using Kaiyuanshe.OpenHackathon.Server.Cache;
 using Kaiyuanshe.OpenHackathon.Server.Storage;
 using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+using Kaiyuanshe.OpenHackathon.Server.Storage.Tables;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading;
@@ -16,7 +17,7 @@ namespace Kaiyuanshe.OpenHackathon.Server.Biz
     }
 
     public interface IDefaultManagementClient<TParameter, TEntity, TOptions>
-        where TEntity : BaseTableEntity
+        where TEntity : BaseTableEntity, new()
         where TParameter : new()
         where TOptions : TableQueryOptions
     {
@@ -34,10 +35,58 @@ namespace Kaiyuanshe.OpenHackathon.Server.Biz
         public ILogger<TManagement> Logger { get; set; }
     }
 
-    //public abstract class DefaultManagementClient<TManagement, TParameter, TEntity> 
-    //    : ManagementClientBase<TManagement>, IManagementClient<TParameter, TEntity>
-    //    where TEntity : BaseTableEntity
-    //    where TParameter : new()
-    //{
-    //}
+    public abstract class DefaultManagementClient<TManagement, TParameter, TEntity, TOptions>
+        : ManagementClientBase<TManagement>, IDefaultManagementClient<TParameter, TEntity, TOptions>
+        where TEntity : BaseTableEntity, new()
+        where TParameter : new()
+        where TOptions : TableQueryOptions
+    {
+        protected abstract TEntity ConvertParamterToEntity(TParameter parameter);
+        protected abstract IAzureTableV2<TEntity> Table { get; }
+        protected virtual bool EnableCache { get; } = true;
+        protected abstract CacheEntryType CacheType { get; }
+
+        public async Task<TEntity> Create(TParameter parameter, CancellationToken cancellationToken)
+        {
+            var entity = ConvertParamterToEntity(parameter);
+            await Table.InsertAsync(entity, cancellationToken);
+            if (EnableCache)
+            {
+                InvalidateCache(entity.PartitionKey);
+            }
+            return entity;
+        }
+
+        public Task Delete(string partitionKey, string rowkey, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TEntity?> Get(string partitionKey, string rowkey, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> List(TOptions options, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TEntity> Update(TEntity existing, TParameter parameter, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #region Cache
+        private string GetCacheKey(string subKey)
+        {
+            return CacheKeys.GetCacheKey(CacheType, subKey);
+        }
+
+        private void InvalidateCache(string subKey)
+        {
+            Cache.Remove(GetCacheKey(subKey));
+        }
+        #endregion
+    }
 }
