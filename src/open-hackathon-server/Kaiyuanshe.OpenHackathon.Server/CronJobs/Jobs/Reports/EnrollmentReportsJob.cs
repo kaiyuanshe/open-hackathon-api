845f4ca2388e43e01ca4293ce7613ca6e40d6341
Junbo Wang <juniwang@microsoft.com>
2022-09-23 22:41:40 +0800

    add cronjob to generate enroll report (#312)

--- src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/CronJobs/Jobs/Reports/EnrollmentReportsJob.cs
+++ src/open-hackathon-server/Kaiyuanshe.OpenHackathon.Server/CronJobs/Jobs/Reports/EnrollmentReportsJob.cs
@@ -1,5 +1,8 @@
-﻿using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+﻿using Kaiyuanshe.OpenHackathon.Server.Storage;
+using Kaiyuanshe.OpenHackathon.Server.Storage.Entities;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,11 +12,41 @@ namespace Kaiyuanshe.OpenHackathon.Server.CronJobs.Jobs.Reports
     {
         protected override string ReportName => "Enrollments";
 
-        protected override Task<IList<dynamic>> GenerateReport(HackathonEntity hackathon, CancellationToken token)
+        protected override async Task<IList<dynamic>> GenerateReport(HackathonEntity hackathon, CancellationToken token)
         {
             IList<dynamic> reports = new List<dynamic>();
 
-            return Task.FromResult(reports);
+            var filter = TableQueryHelper.PartitionKeyFilter(hackathon.Name);
+            await StorageContext.EnrollmentTable.ExecuteQueryAsync(filter, async (enrollment) =>
+            {
+                var user = await StorageContext.UserTable.GetUserByIdAsync(enrollment.UserId, token);
+                if (user == null)
+                    return;
+
+                dynamic record = new ExpandoObject();
+                // user info
+                record.UserId = user.Id;
+                record.UserName = user.Username;
+                record.Nickname = user.Nickname;
+                record.FamilyName = user.FamilyName;
+                record.MiddleName = user.MiddleName;
+                record.GivenName = user.GivenName;
+                record.Email = user.Email;
+                record.Phone = user.Phone;
+                record.Gender = user.Gender;
+                record.Nickname = user.Nickname;
+                // hackathon info
+                record.HackathonName = hackathon.Name;
+                record.HackathonDisplayName = hackathon.DisplayName;
+                // enrollment
+                record.EnrollmentId = enrollment.RowKey;
+                record.EnrollmentStatus = enrollment.Status.ToString();
+                record.Extensions = JsonConvert.SerializeObject(enrollment.Extensions);
+
+                reports.Add(record);
+            }, null, null, token);
+
+            return reports;
         }
     }
 }
